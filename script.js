d4a7d2cb1610346b3838f42924345f86901983f1
shivamkumar8009 <128994962+shivamkumar8009@users.noreply.github.com>
2026-07-27 19:40:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ fetch("index.json")
   .then(res => res.json())
   .then(data => {
     const list = document.getElementById("indexList");
+    const frame = document.getElementById("pdfFrame");
 
     data.forEach(item => {
       const li = document.createElement("li");
       li.textContent = item.title;
 
       li.onclick = function() {
-        document.getElementById("pdfViewer").src =
-          "Trsbook.pdf#page=" + item.page;
+        // CALL function inside iframe (no reload)
+        frame.contentWindow.goToPage(item.page);
       };
 
       list.appendChild(li);
